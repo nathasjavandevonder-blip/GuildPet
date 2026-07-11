5c6f8a82ff3bbdaed8753212d720fcd5d63eb409
Nathasja Vandevonder <nathasjavandevonder@gmail.com>
2026-07-11 13:09:01 +0000

    Add connected travel world map

---
+++ systems/travel/world_map.py
@@ -0,0 +1,35 @@
+WORLD_MAP = {
+    "guild_hall": [
+        "forest",
+        "river",
+    ],
+
+    "forest": [
+        "guild_hall",
+        "mine",
+        "ancient_tree",
+    ],
+
+    "river": [
+        "guild_hall",
+        "forest",
+    ],
+
+    "mine": [
+        "forest",
+        "volcano",
+    ],
+
+    "ancient_tree": [
+        "forest",
+        "snow_peaks",
+    ],
+
+    "snow_peaks": [
+        "ancient_tree",
+    ],
+
+    "volcano": [
+        "mine",
+    ],
+}
